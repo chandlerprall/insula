f13c92d9a07d1ee23fecaa5f2fba9c95bc76a4dd
Michael Merrill <michael@michaelmerrill.io>
2017-10-17 21:58:55 -0600

    Simplify ShallowCompare

--- packages/insula/src/ShallowCompare.js
+++ packages/insula/src/ShallowCompare.js
@@ -1,20 +1,12 @@
-/* global Set, Map*/
-/* eslint curly: "off", complexity: "off" */
+/* eslint curly: "off" */
 
 function isPrimitive(x) {
     return Object(x) !== x;
 }
 
 function isArrayOrObject(x) {
+    if (isPrimitive(x)) return false;
     return x.constructor === Object || x.constructor === Array;
-}
-
-function isMap(x) {
-    return x.constructor === Map;
-}
-
-function isSet(x) {
-    return x.constructor === Set;
 }
 
 function shallowObj(obj1, obj2) {
@@ -39,43 +31,12 @@ function shallowObj(obj1, obj2) {
     return true;
 }
 
-function shallowMap(map1, map2) {
-    if (map1.size !== map2.size) {
-        return false;
-    }
-
-    for (var [key, value] of map1) {
-        if (map2.get(key) !== value) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function shallowSet(set1, set2) {
-    if (set1.size !== set2.size) {
-        return false;
-    }
-
-    for (var val of set1) {
-        if (!set2.has(val)) {
-            return false;
-        }
-    }
-    return true;
-}
-
 export function shallowCompare(a, b) {
     if (a === b) {
         return true;
-    } else if (isPrimitive(a) || isPrimitive(b) || a.constructor !== b.constructor) {
-        return false;
-    } else if (isArrayOrObject(a)) {
+    } else if (isArrayOrObject(a) && isArrayOrObject(b)) {
+        if (a.constructor !== b.constructor) return false;
         return shallowObj(a, b);
-    } else if (isMap(a)) {
-        return shallowMap(a, b);
-    } else if (isSet(a)) {
-        return shallowSet(a, b);
     }
     return false;
 }
--- packages/insula/src/tests/ShallowCompare.jest.js
+++ packages/insula/src/tests/ShallowCompare.jest.js
@@ -1,276 +1,43 @@
 import {shallowCompare} from '../ShallowCompare';
 
-const testShallowCompare = (value, against, result) => {
-    test(`should return ${result} when comparing ${JSON.stringify(value)} against ${JSON.stringify(against)}`, () =>
-        expect(shallowCompare(value, against)).toBe(result));
-};
-
 describe('ShallowCompare', () => {
-    describe('Number', () => {
-        const numberTests = [
-            {value: 0, against: 0, result: true},
-            {value: 0, against: 1, result: false},
-            {value: 0, against: false, result: false},
-            {value: 0, against: undefined, result: false},
-            {value: 0, against: null, result: false},
-            {value: 0, against: '', result: false},
-            {value: 0, against: () => {}, result: false},
-            {value: 0, against: [], result: false},
-            {value: 0, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < numberTests.length; i++) {
-            const number = numberTests[i];
-            const value = number.value;
-            const against = number.against;
-            const result = number.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing {} against []', () => {
+        expect(shallowCompare({}, [])).toBe(false);
     });
 
-    describe('Boolean', () => {
-        const booleanTests = [
-            {value: false, against: false, result: true},
-            {value: false, against: true, result: false},
-            {value: false, against: 0, result: false},
-            {value: false, against: undefined, result: false},
-            {value: false, against: null, result: false},
-            {value: false, against: '', result: false},
-            {value: false, against: () => {}, result: false},
-            {value: false, against: [], result: false},
-            {value: false, against: {}, result: false},
-            {value: true, against: true, result: true},
-            {value: true, against: false, result: false},
-            {value: true, against: 0, result: false},
-            {value: true, against: undefined, result: false},
-            {value: true, against: null, result: false},
-            {value: true, against: '', result: false},
-            {value: true, against: () => {}, result: false},
-            {value: true, against: [], result: false},
-            {value: true, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < booleanTests.length; i++) {
-            const bool = booleanTests[i];
-            const value = bool.value;
-            const against = bool.against;
-            const result = bool.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing {} against null', () => {
+        expect(shallowCompare({}, null)).toBe(false);
     });
 
-    describe('Null', () => {
-        const nullTests = [
-            {value: null, against: null, result: true},
-            {value: null, against: false, result: false},
-            {value: null, against: true, result: false},
-            {value: null, against: undefined, result: false},
-            {value: null, against: 0, result: false},
-            {value: null, against: '', result: false},
-            {value: null, against: () => {}, result: false},
-            {value: null, against: [], result: false},
-            {value: null, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < nullTests.length; i++) {
-            const _null = nullTests[i];
-            const value = _null.value;
-            const against = _null.against;
-            const result = _null.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return true when comparing [1, 2, 3] against [1, 2, 3]', () => {
+        expect(shallowCompare([1, 2, 3], [1, 2, 3])).toBe(true);
     });
 
-    describe('undefined', () => {
-        const undefinedTests = [
-            {value: undefined, against: undefined, result: true},
-            {value: undefined, against: false, result: false},
-            {value: undefined, against: true, result: false},
-            {value: undefined, against: null, result: false},
-            {value: undefined, against: 0, result: false},
-            {value: undefined, against: '', result: false},
-            {value: undefined, against: () => {}, result: false},
-            {value: undefined, against: [], result: false},
-            {value: undefined, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < undefinedTests.length; i++) {
-            const _undefined = undefinedTests[i];
-            const value = _undefined.value;
-            const against = _undefined.against;
-            const result = _undefined.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return true when comparing {a: 1, b: 2, c: 3} against {a: 1, b: 2, c: 3}', () => {
+        expect(shallowCompare({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3})).toBe(true);
     });
 
-    describe('String', () => {
-        const stringTests = [
-            {value: '', against: '', result: true},
-            {value: '', against: 'abc', result: false},
-            {value: '', against: undefined, result: false},
-            {value: '', against: false, result: false},
-            {value: '', against: true, result: false},
-            {value: '', against: null, result: false},
-            {value: '', against: 0, result: false},
-            {value: '', against: () => {}, result: false},
-            {value: '', against: [], result: false},
-            {value: '', against: {}, result: false},
-        ];
-
-        for (let i = 0; i < stringTests.length; i++) {
-            const string = stringTests[i];
-            const value = string.value;
-            const against = string.against;
-            const result = string.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing [1, 2, 3] against [1, 2, 3 4]', () => {
+        expect(shallowCompare([1, 2, 3], [1, 2, 3, 4])).toBe(false);
     });
 
-    describe('Function', () => {
-        const refFunc = () => {};
-        const funcTests = [
-            {value: refFunc, against: refFunc, result: true},
-            {value: () => {}, against: () => {}, result: false},
-            {value: refFunc, against: undefined, result: false},
-            {value: refFunc, against: false, result: false},
-            {value: refFunc, against: true, result: false},
-            {value: refFunc, against: null, result: false},
-            {value: refFunc, against: '', result: false},
-            {value: refFunc, against: 0, result: false},
-            {value: refFunc, against: () => {}, result: false},
-            {value: refFunc, against: [], result: false},
-            {value: refFunc, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < funcTests.length; i++) {
-            const func = funcTests[i];
-            const value = func.value;
-            const against = func.against;
-            const result = func.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing {a: 1, b: 2, c: 3} against {a: 1, b: 2, c: 3, d: 4}', () => {
+        expect(shallowCompare({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 3, d: 4})).toBe(false);
     });
 
-    describe('Array', () => {
-        const refArray = [];
-        const refObj = {};
-        const arrayTests = [
-            {value: refArray, against: refArray, result: true},
-            {value: [], against: [], result: true},
-            {value: [1, 2, 3], against: [1, 2, 3], result: true},
-            {value: [refObj], against: [refObj], result: true},
-            {value: refArray, against: [], result: true},
-            {value: [1, 2], against: [1, 2, 3], result: false},
-            {value: ['a', 'b', 'c'], against: {0: 'a', 1: 'b', 2: 'c'}, result: false},
-            {value: [{}], against: [{}], result: false},
-            {value: refArray, against: undefined, result: false},
-            {value: refArray, against: false, result: false},
-            {value: refArray, against: true, result: false},
-            {value: refArray, against: null, result: false},
-            {value: refArray, against: '', result: false},
-            {value: refArray, against: 0, result: false},
-            {value: refArray, against: () => {}, result: false},
-            {value: refArray, against: {}, result: false},
-        ];
-
-        for (let i = 0; i < arrayTests.length; i++) {
-            const arr = arrayTests[i];
-            const value = arr.value;
-            const against = arr.against;
-            const result = arr.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing [1, 2, 3, 4, 5] against [5, 4, 3, 2, 1]', () => {
+        expect(shallowCompare([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])).toBe(false);
     });
 
-    describe('Object', () => {
-        const refObj = {};
-        const objTests = [
-            {value: refObj, against: refObj, result: true},
-            {value: {}, against: {}, result: true},
-            {value: {a: 1}, against: {a: 1}, result: true},
-            {value: refObj, against: {}, result: true},
-            {value: {}, against: {a: 1}, result: false},
-            {value: {a: 1, b: []}, against: {a: 1, b: []}, result: false},
-            {value: {a: 1, b: {}}, against: {a: 1, b: {}}, result: false},
-            {value: refObj, against: undefined, result: false},
-            {value: refObj, against: false, result: false},
-            {value: refObj, against: true, result: false},
-            {value: refObj, against: null, result: false},
-            {value: refObj, against: '', result: false},
-            {value: refObj, against: 0, result: false},
-            {value: refObj, against: () => {}, result: false},
-            {value: refObj, against: [], result: false},
-        ];
-
-        for (let i = 0; i < objTests.length; i++) {
-            const obj = objTests[i];
-            const value = obj.value;
-            const against = obj.against;
-            const result = obj.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing ["a", "b", "c"] against {0: "a", 1: "b", 2: "c"}', () => {
+        expect(shallowCompare(['a', 'b', 'c'], {0: 'a', 1: 'b', 2: 'c'})).toBe(false);
     });
 
-    describe('Map', () => {
-        const refMap1 = new Map();
-        const refMap2 = new Map().set('test', 'this is a test');
-        const refMap3 = new Map().set('test', 'this is a test');
-        const refMap4 = new Map().set('other test', 'this is another test');
-        const mapTests = [
-            {value: refMap1, against: refMap1, result: true},
-            {value: new Map(), against: new Map(), result: true},
-            {value: new Map(), against: refMap1, result: true},
-            {value: refMap2, against: refMap3, result: true},
-            {value: refMap1, against: refMap2, result: false},
-            {value: refMap1, against: refMap2, result: false},
-            {value: refMap1, against: undefined, result: false},
-            {value: refMap1, against: false, result: false},
-            {value: refMap1, against: true, result: false},
-            {value: refMap1, against: null, result: false},
-            {value: refMap1, against: '', result: false},
-            {value: refMap1, against: 0, result: false},
-            {value: refMap1, against: () => {}, result: false},
-            {value: refMap1, against: [], result: false},
-        ];
-
-        for (let i = 0; i < mapTests.length; i++) {
-            const map = mapTests[i];
-            const value = map.value;
-            const against = map.against;
-            const result = map.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing {0: "a", 1: "b", 2: "c"} against ["a", "b", "c"]', () => {
+        expect(shallowCompare({0: 'a', 1: 'b', 2: 'c'}, ['a', 'b', 'c'])).toBe(false);
     });
 
-    describe('Set', () => {
-        const refSet1 = new Set();
-        const refSet2 = new Set([1, 2, 3]);
-        const refSet3 = new Set([1, 2, 3]);
-        const refSet4 = new Set([2, 3]);
-        const setTests = [
-            {value: refSet1, against: refSet1, result: true},
-            {value: new Set(), against: new Set(), result: true},
-            {value: new Set([1, 2, 3]), against: new Set([1, 2, 3]), result: true},
-            {value: new Set(), against: refSet1, result: true},
-            {value: refSet2, against: refSet3, result: true},
-            {value: refSet1, against: refSet2, result: false},
-            {value: refSet3, against: refSet4, result: false},
-            {value: refSet1, against: undefined, result: false},
-            {value: refSet1, against: false, result: false},
-            {value: refSet1, against: true, result: false},
-            {value: refSet1, against: null, result: false},
-            {value: refSet1, against: '', result: false},
-            {value: refSet1, against: 0, result: false},
-            {value: refSet1, against: () => {}, result: false},
-            {value: refSet1, against: [], result: false},
-        ];
-
-        for (let i = 0; i < setTests.length; i++) {
-            const set = setTests[i];
-            const value = set.value;
-            const against = set.against;
-            const result = set.result;
-            testShallowCompare(value, against, result);
-        }
+    test('should return false when comparing {a: "b", b: "c", c: "a"} against {a: "a", b: "b", c: "c"}', () => {
+        expect(shallowCompare({a: 'b', b: 'c', c: 'a'}, {a: 'a', b: 'b', c: 'c'})).toBe(false);
     });
 });
